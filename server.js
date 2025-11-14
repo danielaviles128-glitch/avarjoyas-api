b58dd135b13fc2ff9cc60b1468991027b44d1a0b
danielaviles128-fallo <danielaviles128@gmail.com>
2025-11-13 23:17:21 -0500

    se arregla configuracion cors en el servidor

--- server.js
+++ server.js
@@ -13,11 +13,13 @@ const pool = new Pool({
 });
 
 const app = express();
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type"]
-}));
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    allowedHeaders: "Content-Type,Authorization",
+  })
+);
 app.use(express.json());
 
 // ---------------------------------------------
